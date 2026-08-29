f51a149480160133457f2e3ec661d0cd076d7287
sabatara98-13 <saba.tara98@gmail.com>
2026-08-30 02:06:10 +0300

    add frontend2

--- Saba/saba_final_task_backend/saba_final_task_backend/obj/Debug/net10.0/saba_final_task_backend.AssemblyInfo.cs
+++ Saba/saba_final_task_backend/saba_final_task_backend/obj/Debug/net10.0/saba_final_task_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("saba_final_task_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68504744c6c751124f606538d8fdb8d26efa3cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7920264097d145dab3f21be1d569718b8758847d")]
 [assembly: System.Reflection.AssemblyProductAttribute("saba_final_task_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("saba_final_task_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
